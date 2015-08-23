74b722665353d9fb559d669e71df405618e8b5f1
Enterthusiast <bernard.damien@gmail.com>
2015-08-23 23:57:07 +0200

    Add node.js & grunt

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+    // 1. All configuration goes here 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        uglify: {
+            options: {
+              mangle: false
+            },
+            my_target: {
+              files: {
+                'dest/output.min.js': ['src/input1.js', 'src/input2.js']
+              }
+            }
+          }
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['uglify']);
+
+};
